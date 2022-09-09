ba6296d3d830d66857521538981c7fb581bea864
JIWON_MIN <mjw3534@gmail.com>
2022-09-09 14:58:01 +0900

    Implement product-related function

---
+++ src/hooks/useActions.js
@@ -0,0 +1,7 @@
+import AppStateContext from "../contexts/AppStateContext";
+import { useContext } from "react";
+
+export default function useActions() {
+  const {addToOrder, remove, removeAll} = useContext(AppStateContext);
+  return {addToOrder, remove, removeAll};
+}
